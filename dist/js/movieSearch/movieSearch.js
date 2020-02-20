bf0071669a875ee2a4b5201f41135e59a175d0e8
Tales F <talesfariasweb@gmail.com>
2020-02-20 16:07:05 +0000

    js code refactor to increase performance, customization and organization

--- dist/js/movieSearch/movieSearch.js
+++ dist/js/movieSearch/movieSearch.js
@@ -1,7 +1,31 @@
-function searchMovie(title){
+// Global template load
+var movieTemplate = loadTemplate();
 
-  // API key
-  var OMDbAPIKey = "de5e1423";
+// API key
+var OMDbAPIKey = "de5e1423";
+
+// Load the template to put into variable
+// this methos is used to load the file only once
+function loadTemplate(){
+
+  var loadedTemplate;
+
+  $.ajax({
+      // todo: check if is possible to have an async load of this file
+      'async': false, 
+      'type': "GET",
+      'global': false,
+      'url': "/js/movieSearch/movieSearch.tpl.html",
+      'data': { 'request': "", 'target': 'arrange_url', 'method': 'method_target' },
+      'success': function (data) {
+          loadedTemplate = data;
+      }
+  });
+  return loadedTemplate;
+} //loadTemplate
+
+// Search the movie and build the list or show alert afterwards
+function searchMovie(title){
 
   // simple query string
   var queryString = "http://omdbapi.com/?apikey="+OMDbAPIKey+"&r=json&s="+ title;
@@ -58,49 +82,87 @@ function bsShowAlert(type, text){
   });
 }
 
+// Builds the movie list after search result
 function moviesBuildList(OMBdMoviesJson){
   var movies = [];
-  var template = "";
-  $.get( "/js/movieSearch/movieSearch.tpl.html").done(function(loadedData){
-    template = loadedData;
-    Object.keys(OMBdMoviesJson.Search).forEach(key => {
-
-      movieData = OMBdMoviesJson.Search[key];
-
-      var movie = template;
-
-      // start to populate the template file
-      movie = movie.replace("{{movie-title}}", movieData.Title);
-
-      // check if has image
-      if(movieData.Poster == "N/A"){
-        movie = movie.replace("{{movie-image}}", "/img/cover-default.png");
-      }else{
-        movie = movie.replace("{{movie-image}}", movieData.Poster);
-      }
-
-      movie = movie.replace("{{movie-description}}", movieData.Year);
-
-      // todo: create pages for each movie
-      movie = movie.replace("{{movie-url}}", "#");
-
-      movies.push(movie);
-
-    });
-
-    // Create output
-    // todo: remove this grid definitions from JS somehow
-    var output = '<div class="row">';
-    Object.keys(movies).forEach(key => {
-      output +='<div class="col-md-6">'+movies[key]+'</div>';
-    });
-    output += '</div>';
-
-    // todo: make this animate
-    $("#movies--list").append(output);
+  Object.keys(OMBdMoviesJson.Search).forEach(key => {
+    movieData = OMBdMoviesJson.Search[key];
+    movies.push(movieBuild(movieData));
   });
 
+  // Create output
+  // todo: remove this grid definitions from JS somehow
+  //       maybe this iteraction shouldn't run twice, but it seems
+  //          wrong to load the json data and construct the output at the same time
+  var output = '<div class="row">';
+  Object.keys(movies).forEach(key => {
+    output +='<div class="col-md-6">'+movies[key]+'</div>';
+  });
+  output += '</div>';
+
+  // todo: make this animate
+  $("#movies--list").append(output);
 }
+
+// Create the movie cover and description with the template 
+function movieBuild(OMBdMovieJson, movieTitle = OMBdMovieJson.Title, movieDescription = OMBdMovieJson.Year){
+
+  movie = movieTemplate;
+
+  // start to populate the template file
+  movie = movie.replace("{{movie-title}}", movieTitle);
+
+  // check if has image
+  if(OMBdMovieJson.Poster == "N/A"){
+    movie = movie.replace("{{movie-image}}", "/img/cover-default.png");
+  }else{
+    movie = movie.replace("{{movie-image}}", OMBdMovieJson.Poster);
+  }
+
+  // todo: add more fields and remove them from template if inexistent
+  movie = movie.replace("{{movie-description}}", movieDescription);
+
+  // todo: create pages for each movie
+  movie = movie.replace("{{movie-url}}", "#");
+
+  return movie;
+}
+
+// Add movie information on DOM elements that have data-imdbid attr
+function pageMoviesInit(){
+  $("[data-imdbid]").each(function(){
+    var $elem = $(this);
+    var imdbid = $elem.attr("data-imdbid");
+
+    // simple query string
+    var queryString = "http://omdbapi.com/?apikey="+OMDbAPIKey+"&r=json&i="+ imdbid;
+
+    $.ajax({
+      url: queryString, 
+      method: 'GET',
+      cache: false,
+      beforeSend: function() {
+        // todo: make loading animation
+        // console.log("loading");
+      }
+    }).done(function(response) {
+
+      if(response.Response == "False") {
+
+      } else {
+
+        var title = response.Title+ " ("+ response.Year + ")";
+
+        $elem.html(movieBuild(response, title, response.Plot));
+      }
+    });
+    // movieBuild(imdbid);
+
+    // console.log(movieBuild(imdbid));
+  });
+}
+
+pageMoviesInit();
 
 $("#movie-search-form").submit(function(){
   searchMovie($(this).find("#movie").val());
